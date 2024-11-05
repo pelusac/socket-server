ae9b66da149f201e6a2d35a10364e0463c831251
pelusac <alegallardo@us.es>
2024-11-05 19:27:11 +0100

    nueva declaración socketio

--- dist/classes/server.js
+++ dist/classes/server.js
@@ -5,13 +5,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const environment_1 = require("../gobal/environment");
+const http_1 = __importDefault(require("http"));
 class Server {
     constructor() {
         this.app = (0, express_1.default)();
         this.port = environment_1.SERVER_PORT;
+        this.httpServer = new http_1.default.Server(this.app);
     }
     start(callback) {
-        this.app.listen(this.port, callback);
+        //this.app.listen( this.port, callback );
+        this.httpServer.listen(this.port, callback);
     }
 }
 exports.default = Server;
