1cfe12dd3122deb31ab8ea887e3f29fa4a1ee15f
pelusac <alegallardo@us.es>
2024-11-05 18:44:39 +0100

    socketio

--- classes/server.ts
+++ classes/server.ts
@@ -1,18 +1,27 @@
 
 import express from 'express';
 import { SERVER_PORT } from '../gobal/environment';
+import socketIO from 'socket.io';
+import http from 'http';
 
 export default class Server{
+
     public app: express.Application;
     public port:number;
+
+    public io: socketIO.Server;
+    private httpServer: http.Server;
 
     constructor( ){ 
         this.app = express();
         this.port = SERVER_PORT;
 
+        this.httpServer = new http.Server( this.app );
+
     }
 
     start ( callback: any ){
-        this.app.listen( this.port, callback );
+        //this.app.listen( this.port, callback );
+        this.httpServer.listen( this.port, callback );
     }
 }    
